481d9c3f7c9dcc2d005a855765aed205a288dced
Fazlul <fkarif.cse@gmail.com>
2020-04-06 00:58:36 +0200

    main home page configure to get home page

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+// use beforeEach route guard to set the language
+router.beforeEach((to, from, next) => {
+  // use the language from the routing param or default language
+  let language = to.params.lang;
+  if (!language) {
+    language = "en";
+  }
+
+  // set the current language for i18n.
+  i18n.locale = language;
+  next();
+});
+
 new Vue({
   i18n,
   router,
